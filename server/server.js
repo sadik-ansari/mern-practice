ec7f6f44fc3f5e107be46b5518481ff3e34364ab
sadik ansari <mohammedsadikansari75@gmail.com>
2026-05-22 19:57:58 +0530

    Learn about how to create server with the help of Express

--- server/server.js
+++ server/server.js
@@ -1,54 +1,12 @@
-import http from 'http'
-import { appendFile } from 'fs/promises'
+import express from 'express';
+const app = express()
 
-const myServer = http.createServer(async (req, res) => {
-
-    if (req.url === '/favicon.ico') return res.end()
-
-    // Create proper URL object
-    const myUrl = new URL(req.url, `http://${req.headers.host}`)
-
-    console.log(req.headers.host)
-
-    // Log request
-    const log = `${Date.now()}: ${req.url} request received\r\n`
-
-    try {
-        await appendFile('./log.txt', log)
-
-        switch (myUrl.pathname) {
-
-            case '/':
-                res.end('Home Page')
-                break
-
-            case '/about':
-
-                // Query parameter
-                const name = myUrl.searchParams
-                console.log(name.get('name'))
-
-                res.end(`Hello ${name}`)
-                break
-
-            case '/contact':
-                res.end('This is contact page')
-                break
-
-            default:
-                res.statusCode = 404
-                res.end('404 Page Not Found')
-        }
-
-    } catch (err) {
-
-        console.log(err)
-
-        res.statusCode = 500
-        res.end('Internal Server Error')
-    }
+app.get('/', (req, res) => {
+    res.send("This is Home page")
 })
 
-myServer.listen(8000, () =>
-    console.log('Server is running on port 8000')
-)
+app.get('/about', (req, res) => {
+    res.send("This is Home page")
+})
+
+app.listen(3000, () => console.log(`server is running on http://localhost:3000`))
