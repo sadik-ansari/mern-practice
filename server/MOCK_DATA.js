16d10e2ae080ddefe3697b70237ae7318040f6e3
sadik ansari <mohammedsadikansari75@gmail.com>
2026-06-02 08:00:14 +0530

    Testing CRUD operations using Postman

--- server/MOCK_DATA.js
+++ server/MOCK_DATA.js
@@ -239,45 +239,5 @@ let users = [
     },
     id: 12,
   },
-  {
-    name: "sadik",
-    username: "sadik",
-    email: "ansari.info",
-    address: {
-      street: "Norberto Crossing",
-      suite: "Apt. 950",
-      city: "South Christy",
-      zipcode: "23505-1337",
-      geo: { lat: "-71.4197", lng: "71.7478" },
-    },
-    phone: "1-477-935-8478 x6430",
-    website: "ola.org",
-    company: {
-      name: "Considine-Lockman",
-      catchPhrase: "Synchronised bottom-line interface",
-      bs: "e-enable innovative applications",
-    },
-    id: 13,
-  },
-  {
-    name: "ansari",
-    username: "ansari",
-    email: "sadik.info",
-    address: {
-      street: "Norberto Crossing",
-      suite: "Apt. 950",
-      city: "South Christy",
-      zipcode: "23505-1337",
-      geo: { lat: "-71.4197", lng: "71.7478" },
-    },
-    phone: "1-477-935-8478 x6430",
-    website: "ola.org",
-    company: {
-      name: "Considine-Lockman",
-      catchPhrase: "Synchronised bottom-line interface",
-      bs: "e-enable innovative applications",
-    },
-    id: 14,
-  },
 ];
 export default users;
--- server/server.js
+++ server/server.js
@@ -14,14 +14,47 @@ app.get('/users', (req, res) => {
 
 app.post('/user', (req, res) => {
     const user = req.body
-    users.push({...user, id: users.length + 1})
+    users.push({ ...user, id: users.length + 1 })
     fs.writeFile('./MOCK_DATA.js', `let users = ${JSON.stringify(users)}; export default users`, (error, result) => {
-        if(error) {
+        if (error) {
             res.send(error)
         } else {
-            res.send({message: 'succes', id: user.length})
+            res.send({ message: 'succes', id: user.length })
         }
     })
+})
+
+app.get('/users/:id', (req, res) => {
+    const id = Number(req.params.id)
+    let user = users.find((user) => user.id === id)
+
+    if (user) {
+        res.send({ message: 'success', user: user })
+    } else {
+        res.send({ message: 'user not found' })
+    }
+})
+
+app.patch('/users/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const updatedUserData = req.body
+    const update = users.map((user) => user.id === id ? { ...user, ...updatedUserData, id } : user)
+
+    fs.writeFile('./MOCK_DATA.js',`let users = ${JSON.stringify(update)}; export default users`, (error) => {
+        console.log(error)
+    })
+    res.send({data: users[users.length - 1]})
+})
+
+app.delete('/users/:id', (req, res) => {
+    const id = Number(req.params.id)
+    const filteredUsers = users.filter((user) => user.id !== id)
+
+    fs.writeFile('./MOCK_DATA.js', `let users = ${JSON.stringify(filteredUsers)}; export default users`, (error) => {
+        console.log(error)
+    })
+
+    res.send(users)
 })
 
 
